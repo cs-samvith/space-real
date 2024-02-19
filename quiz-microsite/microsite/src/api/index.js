af10e1263a00402411d1f7d7472ea4862f8ee677
samvith sachidanandan <samvith.cs@gmail.com>
2024-02-19 17:07:51 -0500

    Update basepath

--- quiz-microsite/microsite/src/api/index.js
+++ quiz-microsite/microsite/src/api/index.js
@@ -1,24 +1,24 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const BASE_URL = process.env.REACT_APP_Quiz_service
+export const BASE_URL = process.env.REACT_APP_Quiz_service;
 
-console.log("BASE_URL",BASE_URL);
+console.log("BASE_URL", BASE_URL);
 
 export const ENDPOINTS = {
-    participant: 'participants',
-    question:'questions',
-    getAnswers : 'questions/getanswers',
-    getServiceSystemInfo : 'systeminfo'
-}
+  participant: "participants",
+  question: "questions",
+  getAnswers: "questions/getanswers",
+  getServiceSystemInfo: "systeminfo",
+};
 
-export const createAPIEndpoint = endpoint => {
-
-    let url = BASE_URL + 'api/' + endpoint + '/';
-    return {
-        fetch: () => axios.get(url),
-        fetchById: id => axios.get(url + id),
-        post: newRecord => axios.post(url, newRecord),
-        put: (id, updatedRecord) => axios.put(url + id, updatedRecord),
-        delete: id => axios.delete(url + id),
-    }
-}
+export const createAPIEndpoint = (endpoint) => {
+  // let url = BASE_URL + '/quizsvc/api/' + endpoint + '/';
+  let url = "/quizsvc/api/" + endpoint + "/";
+  return {
+    fetch: () => axios.get(url),
+    fetchById: (id) => axios.get(url + id),
+    post: (newRecord) => axios.post(url, newRecord),
+    put: (id, updatedRecord) => axios.put(url + id, updatedRecord),
+    delete: (id) => axios.delete(url + id),
+  };
+};
